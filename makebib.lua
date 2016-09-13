20fbae89c295c685e1c5c8913ef8ff43b6247bb6
Michal Hoftich <michal.h21@gmail.com>
2016-09-13 14:03:28 +0200

    Detekce marcu

--- makebib.lua
+++ makebib.lua
@@ -1,14 +1,21 @@
 #!/usr/bin/env texlua
 local marc_parser = require "parsemarc"
+local ris_parser  = require "parseris"
 
 local buff = {}
 for line in io.lines() do
   buff[#buff + 1] = line
 end
 
-local records = marc_parser.parse(table.concat(buff, "\n"))
+local text = table.concat(buff, "\n") 
+
+print("je to marc", marc_parser.detect(text))
+local records = {}
+if marc_parser.detect(text) then
+  records = marc_parser.parse(text)
+end
 -- print(#records)
 for _, rec in ipairs(records) do
-  print(rec["245"][1].a)
+  print((rec["245"] or {{}})[1].a)
 end
 
