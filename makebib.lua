d29ccad7785aa852c52de9c1c48288d0b82e6e69
Michal Hoftich <michal.h21@gmail.com>
2016-09-16 14:12:28 +0200

    Some changes

--- makebib.lua
+++ makebib.lua
@@ -2,6 +2,8 @@
 local marc_parser = require "parsemarc"
 local ris_parser  = require "parseris"
 
+local bib_base    = require "bibbase"
+
 local buff = {}
 for line in io.lines() do
   buff[#buff + 1] = line
@@ -9,13 +11,17 @@ end
 
 local text = table.concat(buff, "\n") 
 
-print("je to marc", marc_parser.detect(text))
 local records = {}
 if marc_parser.detect(text) then
   records = marc_parser.parse(text)
+elseif ris_parser.detect(text) then
+  records = ris_parser.parse(text)
+else
+  print("Can't detect format")
+  print("Usage: ./makebib.lua < bibfile")
 end
 -- print(#records)
 for _, rec in ipairs(records) do
-  print((rec["245"] or {{}})[1].a)
+  -- print((rec["245"] or {{}})[1].a)
 end
 
--- parseris.lua
+++ parseris.lua
@@ -1,4 +1,14 @@
 local parseris = {}
 
+function parseris.detect(text)
+  return text:match("FN%s*[^\n]+")
+end
 
+function parseris.parse(text)
+  local records = {}
+  for line in text:gmatch("([^\n]+)") do
+    print(" *'",line)
+  end
+  return records
+end
 return parseris
